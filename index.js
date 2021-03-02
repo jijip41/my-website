b9ba068c7a2d8a1125cd7875bd5da050d73273c2
Jihye Pak <jiji.pak41@gmail.comn>
2021-03-02 21:35:51 +0000

    added toggle button controler

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 'use strict'
 
+// typing function
+
 const greeting_message = "Hello there I'm Jihye I'm looking for a developer job!";
-
 const greeting_message_container = document.querySelector('.greeting-message');
-
 
 let type_message = '';
 let strArr = greeting_message.toUpperCase().split('').map(elem => elem == " " ? "_" : elem);
 let i = 0;
 
 function typing() {
-
   if(i < strArr.length) {
     greeting_message_container.innerHTML += `${strArr[i]}`;
     i++;
     setTimeout(typing, 70);
   }
-  
 }
-
 typing();
+
+
+// toggle button control
+
+const btn = document.querySelector(".bar");
+const nav = document.querySelector(".nav_container");
+
+btn.addEventListener('click', () => {
+  nav.classList.toggle("nav_active");
+})
