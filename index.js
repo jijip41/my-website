52817182789a0a4b869ab670c65e786ab1826ac9
Jihye Pak <jiji.pak41@gmail.comn>
2021-03-09 19:32:07 +0000

    fixed typing function to reset innerHTML

--- index.js
+++ index.js
@@ -6,16 +6,28 @@ const greeting_message = "Hello there I'm Jihye I'm looking for a developer job!
 const greeting_message_container = document.querySelector('.greeting-message');
 
 let type_message = '';
-let strArr = greeting_message.toUpperCase().split('').map(elem => elem == " " ? "_" : elem);
+
 let i = 0;
 
+
 function typing() {
+  let strArr = greeting_message.toUpperCase().split('').map(elem => elem == " " ? "_" : elem); //HELLO_
+
   if(i < strArr.length) {
     greeting_message_container.innerHTML += `${strArr[i]}`;
     i++;
-    setTimeout(typing, 70);
+    setTimeout(typing, 80);
+
+  if(i === strArr.length) {
+    greeting_message_container.innerHTML = `😊`;
+    i = 0;
   }
+  } 
+
+
 }
+
+
 typing();
 
 
